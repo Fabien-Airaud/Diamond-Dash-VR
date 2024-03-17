9e0809d873000126ba4ee7ec2e9ee8e1a8cf0c26
Fabien-Airaud <airaudfabien@gmail.com>
2024-03-17 19:08:12 -0400

    Can change x position with arrows

--- Assets/Diamond-Dash/Scripts/Player Controller.cs
+++ Assets/Diamond-Dash/Scripts/Player Controller.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
 
+public enum PlayerPosition
+{
+    Left,
+    Middle,
+    Right
+}
+
 public class PlayerController : MonoBehaviour
 {
+    private readonly float xBound = 3.75f;
+    private PlayerPosition playerPosition = PlayerPosition.Middle;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,6 +22,42 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+    }
+
+
+    private void MoveLeft()
+    {
+        if (playerPosition == PlayerPosition.Middle)
+        {
+            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
+            playerPosition = PlayerPosition.Left;
+        }
+        else if (playerPosition == PlayerPosition.Right)
+        {
+            transform.position = new Vector3(0, transform.position.y, transform.position.z);
+            playerPosition = PlayerPosition.Middle;
+        }
+    }
+
+    private void MoveRight()
+    {
+        if (playerPosition == PlayerPosition.Middle)
+        {
+            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
+            playerPosition = PlayerPosition.Right;
+        }
+        else if (playerPosition == PlayerPosition.Left)
+        {
+            transform.position = new Vector3(0, transform.position.y, transform.position.z);
+            playerPosition = PlayerPosition.Middle;
+        }
     }
 }
