8b5fe4f2abb495a997d5500339e4414400b428f8
Fabien-Airaud <airaudfabien@gmail.com>
2024-03-18 19:01:20 -0400

    Move player over movingTime

--- Assets/Diamond-Dash/Scripts/Player Controller.cs
+++ Assets/Diamond-Dash/Scripts/Player Controller.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 
@@ -13,6 +14,8 @@ public class PlayerController : MonoBehaviour
     public XROrigin xROrigin;
 
     private readonly float xBound = 3.75f;
+    private readonly float movingTime = 0.5f;
+    private bool isMoving = false;
     private PlayerPosition playerPosition = PlayerPosition.Middle;
 
 
@@ -25,45 +28,48 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            MoveLeft();
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            MoveRight();
-        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && !isMoving) MoveLeft();
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && !isMoving) MoveRight();
 
         // Update the xROrigin position to match the player's position
         xROrigin.transform.position = new Vector3(transform.position.x, xROrigin.transform.position.y, xROrigin.transform.position.z);
     }
 
 
+    private IEnumerator MovePlayerOverTime(Vector3 targetPosition)
+    {
+        isMoving = true;
+        float elapsedTime = 0;
+        Vector3 startPosition = transform.position;
+
+        while (elapsedTime < movingTime)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, (elapsedTime / movingTime));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+        isMoving = false;
+    }
+
     private void MoveLeft()
     {
-        if (playerPosition == PlayerPosition.Middle)
+        if (playerPosition != PlayerPosition.Left)
         {
-            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
-            playerPosition = PlayerPosition.Left;
-        }
-        else if (playerPosition == PlayerPosition.Right)
-        {
-            transform.position = new Vector3(0, transform.position.y, transform.position.z);
-            playerPosition = PlayerPosition.Middle;
+            // Move the player to the left over movingTime
+            StartCoroutine(MovePlayerOverTime(new Vector3(transform.position.x - xBound, transform.position.y, transform.position.z)));
+            playerPosition--;
         }
     }
 
     private void MoveRight()
     {
-        if (playerPosition == PlayerPosition.Middle)
+        if (playerPosition != PlayerPosition.Right)
         {
-            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
-            playerPosition = PlayerPosition.Right;
-        }
-        else if (playerPosition == PlayerPosition.Left)
-        {
-            transform.position = new Vector3(0, transform.position.y, transform.position.z);
-            playerPosition = PlayerPosition.Middle;
+            // Move the player to the right over movingTime
+            StartCoroutine(MovePlayerOverTime(new Vector3(transform.position.x + xBound, transform.position.y, transform.position.z)));
+            playerPosition++;
         }
     }
 }
