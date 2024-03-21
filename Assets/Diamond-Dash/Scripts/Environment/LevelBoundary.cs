ab9e8e1a66c4fcd00efe1258cbc4ef1583a91590
Fabien-Airaud <airaudfabien@gmail.com>
2024-03-21 19:19:12 -0400

    Create LevelControl object with LevelBoundary script

---
+++ Assets/Diamond-Dash/Scripts/Environment/LevelBoundary.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+
+public enum RoadPosition
+{
+    Left,
+    Middle,
+    Right
+}
+
+public class LevelBoundary : MonoBehaviour
+{
+    public static float xBound = 3.75f;
+}
