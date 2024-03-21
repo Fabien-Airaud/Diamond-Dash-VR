81251302643b6283d2f4045452002f39a32a325a
Fabien-Airaud <airaudfabien@gmail.com>
2024-03-21 19:31:42 -0400

    Can move player on each track of the road

--- Assets/Diamond-Dash/Scripts/Player/PlayerMove.cs
+++ Assets/Diamond-Dash/Scripts/Player/PlayerMove.cs
@@ -1,16 +1,64 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float movingTime = 0.5f;
+    private bool isMoving = false;
+    private RoadPosition playerPosition;
+
+
+    private void Start()
     {
-        
+        transform.position = new Vector3(0, transform.position.y, transform.position.z);
+        playerPosition = RoadPosition.Middle;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+
+    private IEnumerator MovePlayerOverTime(Vector3 targetPosition)
+    {
+        isMoving = true;
+        float elapsedTime = 0;
+        Vector3 startPosition = transform.position;
+
+        while (elapsedTime < movingTime)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / movingTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+        isMoving = false;
+    }
+
+    private void Move()
+    {
+        if (!isMoving)
+        {
+            if (Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal2") > 0)
+            {
+                if (playerPosition != RoadPosition.Right)
+                {
+                    // Move the player to the right over movingTime
+                    StartCoroutine(MovePlayerOverTime(new Vector3(transform.position.x + LevelBoundary.xBound, transform.position.y, transform.position.z)));
+                    playerPosition++;
+                }
+            }
+            else if (Input.GetAxis("Horizontal") < 0 || Input.GetAxis("Horizontal2") < 0)
+            {
+                if (playerPosition != RoadPosition.Left)
+                {
+                    // Move the player to the left over movingTime
+                    StartCoroutine(MovePlayerOverTime(new Vector3(transform.position.x - LevelBoundary.xBound, transform.position.y, transform.position.z)));
+                    playerPosition++;
+                }
+            }
+        }
     }
 }
