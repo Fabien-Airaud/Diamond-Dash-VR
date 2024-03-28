0feba7ad1b9a1676d39443311131dea9b9bdc150
Fabien-Airaud <airaudfabien@gmail.com>
2024-03-28 18:48:43 -0400

    Change collect diamond to change diamond count and get audio source if none

--- Assets/Diamond-Dash/Scripts/Collectables/CollectDiamond.cs
+++ Assets/Diamond-Dash/Scripts/Collectables/CollectDiamond.cs
@@ -3,12 +3,23 @@ using UnityEngine;
 public class CollectDiamond : MonoBehaviour
 {
     public AudioSource diamondFX;
+    private CollectableControl collectableControl;
+
+
+    void Start()
+    {
+        GameObject levelControl = GameObject.Find("LevelControl");
+        if (diamondFX == null) diamondFX = levelControl.GetComponentsInChildren<AudioSource>()[0];
+        collectableControl = levelControl.GetComponent<CollectableControl>();
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             diamondFX.Play();
+            collectableControl.AddDiamond(1);
             Destroy(gameObject);
         }
     }
