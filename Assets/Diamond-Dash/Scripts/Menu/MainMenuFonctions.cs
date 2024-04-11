6d9fb433205c83123033c04fcdb6435949bd841e
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-10 23:18:22 -0400

    Create menu script

---
+++ Assets/Diamond-Dash/Scripts/Menu/MainMenuFonctions.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuFonctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
