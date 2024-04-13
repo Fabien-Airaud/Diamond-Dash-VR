7f88e0010999caf4485f6e5d2ffebe7a7d6195c1
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-13 00:13:46 -0400

    Can generate 1 pack at start

--- Assets/Diamond-Dash/Scripts/Environment/GenerateLevel.cs
+++ Assets/Diamond-Dash/Scripts/Environment/GenerateLevel.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GenerateLevel : MonoBehaviour
@@ -6,10 +8,29 @@ public class GenerateLevel : MonoBehaviour
     public GameObject[] sectionPrefabs;
     public float sectionSize = 187.5f;
     public int nbSections = 5;
+
+    public float packsMinDistance = 10f;
+    public GameObject[] pack1LaneAvoidable;
+    public GameObject[] pack1LaneNonAvoidable;
+    public GameObject[] pack2LanesAvoidable;
+    public GameObject[] pack2LanesNonAvoidable;
+    public GameObject[] pack3LanesAvoidable;
+    public GameObject[] pack3LanesNonAvoidable;
     
     private GameObject player;
     private Queue<GameObject> sections;
     private float middleSectionZPosition;
+    private float lastPackZPosition;
+
+    private enum PackChoice
+    {
+        PACK1_LANE,
+        PACK2_LANES,
+        PACK3_LANES,
+        PACK2_1_LANE,
+        PACK3_1_LANE_MIN1_AVOIDABLE/*,
+        PACK3_1_2_LANE_MIN1_AVOIDABLE*/
+    }
 
 
     void Start()
@@ -28,6 +49,7 @@ public class GenerateLevel : MonoBehaviour
 
         sections = new();
         GenerateInitialSections();
+        GeneratePack();
     }
 
     void Update()
@@ -43,7 +65,7 @@ public class GenerateLevel : MonoBehaviour
         // Generate the first sections (back)
         for (int i = middle; i > 0; i--)
         {
-            int randomIndex = Random.Range(0, sectionPrefabs.Length);
+            int randomIndex = UnityEngine.Random.Range(0, sectionPrefabs.Length);
             sections.Enqueue(Instantiate(sectionPrefabs[randomIndex], new Vector3(0, 0, -i * sectionSize), Quaternion.identity));
         }
 
@@ -56,7 +78,7 @@ public class GenerateLevel : MonoBehaviour
         // Generate the last sections (front)
         for (int i = 1; i < middle + 1; i++)
         {
-            int randomIndex = Random.Range(0, sectionPrefabs.Length);
+            int randomIndex = UnityEngine.Random.Range(0, sectionPrefabs.Length);
             sections.Enqueue(Instantiate(sectionPrefabs[randomIndex], new Vector3(0, 0, i * sectionSize), Quaternion.identity));
         }
     }
@@ -73,6 +95,65 @@ public class GenerateLevel : MonoBehaviour
 
             // Update the middle section z position
             middleSectionZPosition += sectionSize;
+        }
+    }
+
+    private PackChoice GetPackChoice()
+    {
+        PackChoice[] packChoices = (PackChoice[])Enum.GetValues(typeof(PackChoice));
+        int random = UnityEngine.Random.Range(0, packChoices.Length);
+        return packChoices[random];
+    }
+
+    private RoadPosition GetRandomRoadPosition(RoadPosition[] except)
+    {
+        List<RoadPosition> roadPositions = new((RoadPosition[])Enum.GetValues(typeof(RoadPosition)));
+        foreach (RoadPosition roadPosition in except) roadPositions.Remove(roadPosition);
+        int random = UnityEngine.Random.Range(0, roadPositions.Count);
+        return roadPositions[random];
+    }
+
+    private void GeneratePack1Lane()
+    {
+        List<GameObject> pack = new(pack1LaneAvoidable);
+        pack.AddRange(pack1LaneNonAvoidable);
+        if (pack.Count == 0) return;
+
+        float zPosition = lastPackZPosition + packsMinDistance;
+        RoadPosition roadPosition = GetRandomRoadPosition(new RoadPosition[0]);
+        GameObject gameObject = pack[UnityEngine.Random.Range(0, pack.Count)];
+        Vector3 position = new Vector3(LevelBoundary.laneSize * (int)roadPosition, 0, zPosition) + gameObject.transform.position;
+
+        Instantiate(gameObject, position, gameObject.transform.rotation);
+        lastPackZPosition = zPosition;
+    }
+
+    private void GeneratePack()
+    {
+        if (player.transform.position.z + nbSections * sectionSize < lastPackZPosition) return;
+
+        PackChoice packChoice = PackChoice.PACK1_LANE;
+        //PackChoice packChoice = GetPackChoice();
+        switch (packChoice)
+        {
+            case PackChoice.PACK1_LANE:
+                GeneratePack1Lane();
+                break;
+            //case PackChoice.PACK2_LANES:
+            //    GeneratePack2Lanes();
+            //    break;
+            //case PackChoice.PACK3_LANES:
+            //    GeneratePack3Lanes();
+            //    break;
+            //case PackChoice.PACK2_1_LANE:
+            //    GeneratePack2Lanes1Lane();
+            //    break;
+            //case PackChoice.PACK3_1_LANE_MIN1_AVOIDABLE:
+            //    GeneratePack3Lanes1LaneMin1Avoidable();
+            //    break;
+            default:
+                Debug.LogError("Pack choice not found");
+                break;
         }
     }
 }
