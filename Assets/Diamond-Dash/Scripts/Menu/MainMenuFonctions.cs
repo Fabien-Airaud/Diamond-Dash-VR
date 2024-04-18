62007a76d4d338d805140be75d81e45036117cf8
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-17 22:53:19 -0400

    Remove unused functions

--- Assets/Diamond-Dash/Scripts/Menu/MainMenuFonctions.cs
+++ Assets/Diamond-Dash/Scripts/Menu/MainMenuFonctions.cs
@@ -3,18 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuFonctions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
